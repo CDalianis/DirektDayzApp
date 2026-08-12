b1087e4c8433a5e2aaf2f493b46a07033e05c9be
CDalianis <christos_dl@hotmail.com>
2026-08-12 12:31:44 +0300

    feat: add QuantityChangeReason enum for inventory adjustments

---
+++ honeyapp-backend/src/main/java/com/honeyapp/model/QuantityChangeReason.java
@@ -0,0 +1,9 @@
+package com.honeyapp.model;
+
+public enum QuantityChangeReason {
+    NEW_HARVEST,
+    INVENTORY_CORRECTION,
+    OFFLINE_SALE,
+    DAMAGE_OR_SPOILAGE,
+    SAMPLES_OR_PROMOTION
+}
